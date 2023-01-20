adc9a7ccc6e2ccfc4e9b99eb798a6bfb0bd05bee
Riya Choudhary <riya.ch2309@gmail.com>
2023-01-20 23:16:06 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
-            navbar
+            <div className={styles.item}>
+                <div className={styles.callButton}>
+                    <Image src="/img/tele1.png" alt="" width="32" height="32" />
+                </div>
+                <div className={styles.texts}>
+                    <div classname={styles.text}>ORDER NOW!</div>
+                    <div classname={styles.text}>012 345 678</div>
+                </div>
+            </div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
         </div>
-    )
+    );
 }
 
 export default Navbar
